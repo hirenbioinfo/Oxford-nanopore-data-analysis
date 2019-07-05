49810f152d510dd0480c11feda3c6abcf1df83a1
Dr. Hiren Ghosh <hiren.ghosh@gmail.com>
2019-07-05 09:30:36 +0200

    Update run_prokka_HPC.sh

--- run_prokka_HPC.sh
+++ run_prokka_HPC.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-#SBATCH -c 8
+#SBATCH --job-name=Prokka assembly 
+#SBATCH --nodes= 8
+#SBATCH --mem= 65536
 #SBATCH -p long
-#SBATCH --mail-type=END
+#SBaTCh --mail-type=begin
+#SBATCH --mail-type=end
 #SBATCH --mail-user=hig18@dsmz.de
 # here starts your actual program call/computation
 #
+start_time=$(date +%s)
+echo "Initial time " $start_time
+
 for F in *.fasta; do 
   N=$(basename $F .fasta) ; 
   prokka --cpus 8 --force --locustag $N --kingdom Bacteria \
   --outdir $N --prefix $N  $F ; 
 done
+
+stop_time=$(date +%s)
+echo "Final time " $stop_time
+execution_time=$(expr $stop_time - $start_time)
+echo -e "Execution time " $execution_time " seconds \n "
+echo -e "\t \t" $(($execution_time/60)) " minutes \n"
+echo -e "\t \t" $(($execution_time/3600)) " hours \n"
