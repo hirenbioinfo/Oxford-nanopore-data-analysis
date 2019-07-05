47902eb19f8a7b5762d5b5cd85ac4531b045e0d3
Dr. Hiren Ghosh <hiren.ghosh@gmail.com>
2019-07-05 09:28:34 +0200

    Update run_pilon_HPC.sh

--- run_pilon_HPC.sh
+++ run_pilon_HPC.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-#SBATCH -c 8
-#SBATCH --mem 65536
+#SBATCH --job-name=Prokka assembly 
+#SBATCH --nodes= 8
+#SBATCH --mem= 65536
 #SBATCH -p long
-#SBATCH --mail-type=END
+#SBaTCh --mail-type=begin
+#SBATCH --mail-type=end
 #SBATCH --mail-user=hig18@dsmz.de
 # here starts your actual program call/computation
 #
-#echo "START TIME: " `date`
+
+start_time=$(date +%s)
+echo "Initial time " $start_time
 
 pilon --genome Canu_H1099.fasta --frags Canu_H1009.bam --output pilon1 --fix all --mindepth 0.5 --changes --verbose --threads 8
 
- 
-
+stop_time=$(date +%s)
+echo "Final time " $stop_time
+execution_time=$(expr $stop_time - $start_time)
+echo -e "Execution time " $execution_time " seconds \n "
+echo -e "\t \t" $(($execution_time/60)) " minutes \n"
+echo -e "\t \t" $(($execution_time/3600)) " hours \n"
 
 
 
